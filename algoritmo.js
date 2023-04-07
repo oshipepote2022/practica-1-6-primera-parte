cd28ebd9f91290a2151dd25c41a39d653411dd8e
Gustavo <gustavo_r69@yahoo.com.ar>
2023-04-07 18:34:42 -0300

    envio de practica 1-6 primera parte

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+let gatofavorito = prompt("cual es la raza de tu gato favorito?");
+
+console.log( gatofavorito);
+document.querySelector("body").innerHTML = "<h1>"+ gatofavorito + " </h1>";
+document.write( "<h2>"+ gatofavorito + " </h2>" );

7a15c0d52e20d1eb62611b881e9bc570ad623da7
Gustavo <gustavo_r69@yahoo.com.ar>
2023-04-07 20:28:56 -0300

    entrega practica 1-6 segunda parte

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,18 @@
-let gatofavorito = prompt("cual es la raza de tu gato favorito?");
+let numeroEntero = prompt("ingresar el tipo de dato integer/entero:");
+let booleano = prompt("Ingresar el tipo de dato booleano:");
+let numeroDecimal = prompt("Ingresar un numero decimal:");
+let caracter = prompt("Ingresa un caracter");
+let palabra = prompt("Ingresa una palabra");
 
-console.log( gatofavorito);
-document.querySelector("body").innerHTML = "<h1>"+ gatofavorito + " </h1>";
-document.write( "<h2>"+ gatofavorito + " </h2>" );
+
+document.write("<h1> Entero: "+ numeroEntero +"</h1>");
+document.write("<h2> Booleano: "+ booleano +"</h2>");
+document.write("<h3> Decimal:  "+ decimal +"</h3>");
+document.write("<h4> Caracter: "+ caracter +"</h4>");
+document.write("<h5> Palabra(conjunto de caracteres): "+ palabra +"</h5>");
+
+
+
+
+
+
